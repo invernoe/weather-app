b1697e6750b7e64669dbdd81937a5fca435b5609
invernoe <mahmoudk221999@gmail.com>
2024-01-04 21:59:40 +0200

    finalize fetching logic

--- js/index.js
+++ js/index.js
@@ -1,18 +1,48 @@
-const apiKey = "83fb001499a44a3b96b04403240301"
-const baseUrl = "http://api.weatherapi.com/v1/current.json?key=&q=London"
-const defaultCity = "London"
-q=48.8567,2.3508
+const apiKey = "83fb001499a44a3b96b04403240301";
+const numberOfDays = 3;
+const baseUrl = "http://api.weatherapi.com/v1/forecast.json?key=" + apiKey + "&days=" + numberOfDays;
+const defaultCity = "London";
+
+let locationTextInput = document.querySelector("#locationTextInput");
+//data to be modified
+const refreshedData = {
+    currentDay: document.querySelector("#weather-content-date-1"),
+    dates: document.querySelectorAll("[id^='weather-content-day-']"),
+
+}
 
 getLocation();
+fetchWeatherData(defaultCity);
+
+async function fetchWeatherData(queryLocation) {
+  let response = await fetch(baseUrl + "&q=" + queryLocation);;
+  let finalResponse = await response.json();
+
+  console.log(finalResponse);
+  refreshDisplay(finalResponse);
+}
 
 function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-      x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(saveCoords);
   }
+}
 
-function showPosition(position) {
-    console.log(position)
+function saveCoords(position) {
+  currentCoords = position.coords.latitude + "," + position.coords.longitude;
+  //fetch data based on current coordinates
+  fetchWeatherData(currentCoords);
+}
+
+function fetchWeatherDataTextInput() {
+    let city = locationTextInput.value();
+    fetchWeatherData(city);
+}
+
+function refreshDisplay(response) {
+    for (let i = 0; i < numberOfDays; i++) {
+        let date = new Date(response.forecast.forecastday[i].date).toLocaleDateString('en-US', {weekday: "long"});
+        
+    }
+    
 }
